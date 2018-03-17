0a39610be98ebc16e314316e993c29547d0b0262
mgoyal05 <mgoyal05.mg@gmail.com>
2018-03-17 21:46:16 +0530

    Basic Layout with sadhna card format

--- app/src/main/java/com/example/zemoso/sadhnacard/MainActivity.java
+++ app/src/main/java/com/example/zemoso/sadhnacard/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.zemoso.sadhnacard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mNarottamaButton;
+
+    private Button mSyamanandaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSyamanandaButton = findViewById(R.id.start_syamananda_button);
+
+        mSyamanandaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SavedCardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mNarottamaButton = findViewById(R.id.start_narottama_button);
+
+        mNarottamaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NarottamActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/zemoso/sadhnacard/NarottamActivity.java
@@ -0,0 +1,54 @@
+package com.example.zemoso.sadhnacard;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NarottamActivity extends AppCompatActivity {
+
+    Button buttonSend;
+    EditText textPhoneNo;
+    EditText textSMS;
+    private Button mSendMessageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_narottam);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
+        textSMS = (EditText) findViewById(R.id.editTextSMS);
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String phoneNo = textPhoneNo.getText().toString();
+                String sms = textSMS.getText().toString();
+
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS Sent!",
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again later!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
